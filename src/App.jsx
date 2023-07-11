7b65fe9cfd37297edfa3fdd6fcbfeeb83ed2d7d4
FranAlamo <alamofrancis@gmail.com>
2023-07-11 17:38:22 -0300

    search

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Countries from "./Components/Countries";
 import Filter from "./Components/Filter";
 import Header from "./Components/Header";
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Filter />
       <Countries />
-    </>
+    </Router>
   );
 }
 
