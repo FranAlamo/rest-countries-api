4e0c9832c1ff26cbb4675f81c9b0cbacd0b260ac
FranAlamo <alamofrancis@gmail.com>
2023-07-11 15:14:50 -0300

    filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Countries from "./Components/Countries";
+import Filter from "./Components/Filter";
+import Header from "./Components/Header";
 function App() {
   return (
     <>
+      <Header />
+      <Filter />
       <Countries />
     </>
   );
